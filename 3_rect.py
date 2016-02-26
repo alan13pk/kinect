c97370702603886e3509fedf7fd0e05df1ed2c9c
ubuntu <com5620081@gmail.com>
2016-02-26 11:38:54 +0700

    3_rect: take picture

--- 3_rect.py
+++ 3_rect.py
@@ -3,19 +3,23 @@ import freenect
 import cv2
 import numpy as np
 import time
+from random import randint
 threshold = 100
 current_depth = 650
 adj = 8
 zoomy = 1.1
 zoomx = 1.13
 state_rect = "start_rect"
-state_num = "start_num"
-rectxy = [[2,2],[102,2],[202,2]]
-rectw = 100
-recth = 100
+state_num = "stop_num"
+rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]
+rectw = 60
+recth = 80
 number_rect = 0
-check_rect = rectw*recth/2
+check_rect = rectw*recth*2/5
 sum_boolean = 0
+stop_rectxy = [200,300]
+count_down = 5
+start_time = 0
 # Create a detector with the parameters
 ver = (cv2.__version__).split('.')
 #print ver
@@ -62,20 +66,25 @@ def draw_rect(frame,depth):
     global number_rect
     global state_rect
     global start_time
+    global state_num
+    global rectxy
     rectx = rectxy[number_rect][0]
     recty = rectxy[number_rect][1]
     sum_boolean =  check_boolean(depth,rectx,recty) 
     if(state_rect == "start_rect"):
-        #print("asd")
         if(sum_boolean > check_rect):
-            cv2.rectangle(frame,(rectx,recty),(rectx+rectw,rectx+recth),(0,255,0),3)
+            cv2.rectangle(frame,(rectx,recty),(rectx+rectw,recty+recth),(0,255,0),3)
             if(time.time() - start_time >= 1) :
                 number_rect = number_rect+1
                 if(number_rect < len(rectxy)):
+                    rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]
                     state_rect = "draw_rect"
                 else:
                     number_rect = 0
+                    start_time = time.time()
+                    rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]
                     state_rect = "end_rect"
+                    state_num = "start_num"
         else:
             cv2.rectangle(frame,(rectx,recty),(rectx+rectw,recty+recth),(0,0,255),3)
             start_time = time.time()
@@ -83,7 +92,8 @@ def draw_rect(frame,depth):
     elif(state_rect == "draw_rect"):
         cv2.rectangle(frame,(rectx,recty),(rectx+rectw,recty+recth),(0,0,255),3)
         if(time.time() - start_time >= 3):
-            number_rect = 0        
+            number_rect = 0
+            rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]        
             state_rect = "start_rect"
         elif(sum_boolean > check_rect):
             start_time = time.time()
@@ -95,17 +105,66 @@ def draw_rect(frame,depth):
             number_rect = number_rect+1
             if(number_rect < len(rectxy)):         
                 state_rect = "draw_rect"
+                rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]
             else:
                 number_rect = 0
+                start_time = time.time()
                 state_rect = "end_rect"
+                state_num = "start_num"
+                rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]
         elif(not(sum_boolean > check_rect)):
             number_rect = 0
+            rectxy = [[randint(2,102),randint(2,302)],[randint(202,302),randint(2,302)],[randint(402,502),randint(2,302)]]
             state_rect = "start_rect"
     return frame
 
 def draw_num(frame,depth):
     global state_num
-    
+    global state_rect
+    global start_time
+    global count_down
+    rectx = stop_rectxy[0]
+    recty = stop_rectxy[1]
+    sum_boolean =  check_boolean(depth,rectx,recty) 
+    if(state_num == "start_num"):
+        cv2.rectangle(frame,(rectx,recty),(rectx+rectw,recty+recth),(0,255,0),3)
+        cv2.putText(crop_frame,str(count_down), (300,200), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255),5)
+        if(sum_boolean > check_rect):
+            start_time = time.time()
+            state_num = "check_reset"
+        if(time.time() - start_time >= 1):
+            start_time = time.time()
+            count_down = count_down-1
+            if(count_down == 0):
+                count_down = 5
+                state_rect = "start_rect"
+                state_num = "stop_num"
+                capVidio()
+                start_time = time.time()
+    elif(state_num == "check_reset"):
+        cv2.rectangle(frame,(rectx,recty),(rectx+rectw,recty+recth),(0,0,255),3)
+        cv2.putText(crop_frame,"RESET", (300,200), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,255),3)
+        count_down = 5
+        if(sum_boolean > check_rect):
+            if(time.time() - start_time >= 1):
+                state_num = "stop_num"
+                state_rect = "start_rect"
+        else:
+            state_num = "start_num"
+    return frame
+
+def capVidio():
+    cap = cv2.VideoCapture(1)
+    value,image = cap.read()
+    time.sleep(1)
+    #image = get_video()
+    #frame=cv2.flip(image,1)
+    #big_frame = cv2.resize(frame,(0,0), fx=zoomx, fy=zoomy)
+    #crop_frame = big_frame[480*(zoomx-1)/2:480+480*(zoomx-1)/2,640*(zoomy-1)/2:640+640*(zoomy-1)/2]
+    name = time.strftime("%d_%b_%Y_%H_%M_%S")
+    name = "/home/ubuntu/kinect_test/pic/"+name+".jpg"
+    #cv2.imwrite(name,crop_frame)
+    cv2.imwrite(name,image)
 
 
 if __name__ == "__main__":
@@ -120,6 +179,11 @@ if __name__ == "__main__":
 
         #draw rect
         rect_frame = draw_rect(contours_frame,thresh)
+
+        rect_frame = draw_num(rect_frame,thresh)
+
+        #cv2.namedWindow("RGB image", cv2.WND_PROP_FULLSCREEN)
+        #cv2.setWindowProperty("RGB image", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
 
         cv2.imshow('RGB image',rect_frame)
         #cv2.imshow('depth img',thresh )
